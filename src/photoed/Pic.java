1a9929929e35f87531569a55829cee2e95dd5a66
spitlord <darklaboratory1@gmail.com>
2017-12-10 05:50:05 -0500

    Update Pic.java
    Added some comments, but didn't change anything

--- src/photoed/Pic.java
+++ src/photoed/Pic.java
@@ -26,6 +26,11 @@ public class Pic {
 	public int size(){ return width*height; }
 
 	//CONSTRUCTORS
+        // It is bad practice to have multiple constructors (look up telescoping constructors)
+        // Instead, I propose that we introduce initialization functions for different cases
+        
+
+        
 	public Pic(String filename) throws IOException {
 		this(ImageIO.read(new File(filename)));
 	}
@@ -48,6 +53,7 @@ public class Pic {
 		);
 	}
 	public Pic(int width, int height, byte[] buffer){
+            
 		this.width  = width;
 		this.height = height;
 		this.buffer = buffer;
@@ -57,6 +63,8 @@ public class Pic {
 	public void write(File file, String format) throws IOException {
 		ImageIO.write(
 			createRGBImage(
+                                // word "this" in here is redundant
+
 				this.buffer,
 				this.width,
 				this.height
@@ -65,7 +73,47 @@ public class Pic {
 			file
 		);
 	}
+        
+        // word this is only needed in cases of construtors
+        // e.g:
+        //  Dog (String name, int legs) {
+        //         this.name = name;
+        //         this.legs = legs;
+        //  }
+        
+        //  Now you can just use name and legs without word this in methods
+        
+        
+        
+        // public void amputateLeg() { if (legs > 0) legs = legs - 1; } // pretty
+        // public void amputateLeg() { if (this.legs > 0) this.legs = this.legs - 1; } // ugly
+        
+        
+        //
+        //   private void  openMouth() { ... }
+        //   private void  makeSound() { ... }
+       
+        //   public void bark() { 
+        //         this.openMouth();    // redundant
+        //         this.makeSound();    // redundant
+        //   }
+        //
+        
+        //   public void bark() { 
+        //        openMouth();          // still refering to this
+        //        makeSound();          // 
+        //   }
+        
+       
+        //  Adding "this" makes the code look cumbersome and complicated.
+        
+        
+        
+       
 	public void write(String filename) throws IOException {
+            
+           
+            
 		this.write(
 			new File(filename),
 			filename.split("\\.(?=[^\\.]+$)")[1]
@@ -73,6 +121,27 @@ public class Pic {
 	}
 
 	//PER-CHANNEL GET/SET
+        // I feel like words "get" and "set" should not be resereved
+        // We should rename these functions for more clarity.
+        // Everything should have an explicit name.
+        // Put as much meaning in every name.
+        // Also for methods with few arguments. It's kinda confusing to write
+        // each argument in a new line.
+        // I think it  should only be done if there are too many arguments.
+        // Every line is for a its own idea. One idea shouldn't occupy more space
+        // than it needs.
+        //                    return this.get(
+	//		                          channel,
+	//		                          this.index(x,y)   /// ugly
+	//	                             )
+        //
+        //      return this.get(channel, this.index(x,y));          /// pretty
+     
+        
+        
+        // Also, lets agree to comment on all the functions that we create.
+        // It will help a lot for both of us to understand what we mean;
+        
 	public double get(int channel, int index){
 		return doubleOf(this.buffer[channel+index*3]);
 	}
@@ -94,6 +163,9 @@ public class Pic {
 	}
 
 	// RGB GET/SET
+        // 
+
+        
 	public double[] getRGB(int index){
 		return new double[]{
 			get(0,index),
@@ -121,6 +193,9 @@ public class Pic {
 	private int index(int x, int y){
 		return x + this.width * y;
 	}
+        
+        
+        // double_ XD this is such a bad name
 	private static byte byteOf(double double_){
 		if(double_ > 1){
 			return (byte) 0xff;
