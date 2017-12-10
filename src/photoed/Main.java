7d32dcf27fbc457ede0882f65cc852ca8fbdfd0f
!!!XD <mark.koshkin@stonybrook.edu>
2017-12-10 06:20:26 -0500

    Deleted garbage from main

--- src/photoed/Main.java
+++ src/photoed/Main.java
@@ -51,94 +51,7 @@ public class Main {
 
 		printRuntime(start_time);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-	// the path to the image on your machine
-	private static String IMAGE_PATH = "/Users/XDXD/Downloads/IMG_2528-1.JPG";
-	private static String OUTPUT_PATH = "/Users/XDXD/Desktop/genFil/";
-	private static String OUTPUT_NAME = "evilArrayOfColours";
-
-	// to avoid collisions when outputting new images in the same folder
-	private static boolean  MAKE_UP_NAME = true;
-	private static int      MAKE_UP_NAME_CONSTANT = 15;
-
-	// byte arrays in which images are stored
-	private static byte[] ORIGINAL_IMAGE;
-	private static byte[] COPY_OF_ORIGINAL;
-	private static byte[] FILTERED_IMAGE;
-
-	// creates a copy during initialization
-	// it is stored in COPY_OF_ORIGINAL
-	private static boolean  CREATE_COPY = false;
-
-
-
-	// dimentions of the original image
-	// will be initialized automatically
-	private static int IMAGE_WIDTH;
-	private static int IMAGE_HEIGHT;
-
-	// Runtime tools
-	private static long START_TIME;
-
-
-
-    /* INITILIZING IMAGE TOOLS  */
-
-	// returns the image in the form of numbers
-	// in the end, colours are just numbers
-
-	public static byte[] extractBytes (BufferedImage img) throws IOException {
-
-		 // get DataBufferBytes from Raster
-		 WritableRaster raster = img.getRaster();
-		 DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
-
-		 return (data.getData());
-	}
-
-	private static byte[] initializeImage(String path, boolean createCopy) {
-		byte[] byteArray;
-		BufferedImage bufferedImage;
-		try {
-			bufferedImage = ImageIO.read(new File(IMAGE_PATH));
-
-			IMAGE_WIDTH  = bufferedImage.getWidth();
-			IMAGE_HEIGHT = bufferedImage.getHeight();
-
-			byteArray = extractBytes(bufferedImage);
-			if (createCopy) {COPY_OF_ORIGINAL = createCopy(byteArray);}
-			return byteArray;
-		} catch (IOException e) {
-			System.out.println("Could not initialize the image the bytes.");
-			return null;
-		}
-	}
-
-
-	// if only one image to be processed multiple times
-	// then it makes sence to avoid opening it every loop
-	// this can be done by creating a copy of byte array
-
-	private static byte[] createCopy(byte[] image) {
-		byte[] copy = Arrays.copyOf(image, image.length);
-		return copy;
-	}
-
-	 /* OUTPUTTING IMAGE TOOLS  */
-
-	// creates a string of numbers to add to the name
-	// in order to avoid collisions
+	
 
 	private static String makeUpName(int howManyCharacters) {
 		String suffix = "_";
@@ -148,33 +61,6 @@ public class Main {
 		return suffix;
 		}
 
-	private static BufferedImage createRGBImage(byte[] bytes, int width, int height) {
-	    DataBufferByte buffer = new DataBufferByte(bytes, bytes.length);
-	    ColorModel cm = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_sRGB), new int[]{8, 8, 8}, false, false, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
-	    return new BufferedImage(cm, Raster.createInterleavedRaster(buffer, width, height, width * 3, 3, new int[]{0, 1, 2}, null), false, null);
-	}
-
-	private static void outputImage(byte[] filteredImage, String path, String name, int number, boolean makeUpName) {
-		OutputStream outputStream = null;
-		BufferedImage resultingImage;
-	    try {
-	    	if (makeUpName) {
-	    		outputStream = new BufferedOutputStream(
-					new FileOutputStream(OUTPUT_PATH + OUTPUT_NAME + makeUpName(MAKE_UP_NAME_CONSTANT) + ".jpg")
-					);
-			} else {
-				outputStream = new BufferedOutputStream(
-					new FileOutputStream(OUTPUT_PATH + OUTPUT_NAME + ".jpg")
-					);
-			}
-			resultingImage = createRGBImage(filteredImage, IMAGE_WIDTH, IMAGE_HEIGHT);
-			ImageIO.write(resultingImage, "jpg", outputStream);
-
-		} catch (Exception e) {
-			System.out.println("Failed to output the image.");
-		}
-
-	}
 
 
 	// prints how long it took the program to complete
